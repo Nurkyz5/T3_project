0b978d52934c4d4d5d385efa10a5c09a99991c61
Nurkyz5 <nurrkyz@gmail.com>
2026-05-03 21:40:00 +0600

    first lesson

---
+++ Ai_courses/Lectures/python_1.py
@@ -0,0 +1,28 @@
+# ======Types of data========
+# ====================================
+# Changeable
+#   dict - key:value wherea key is not changeable
+#   list
+#   set {1,2,3,'hello'} 
+
+# Not changeable
+#   zahlen numbers - int =1 2 3
+#   float = 1.2 3.4
+#   decimal = 1.3
+#   str 
+#   tuple - множество - (1,2,3)
+        # ex: a=(1,2,3)
+# bool - True,False
+
+
+
+element=10
+element2=10
+print(id(element))
+print(id(element2))
+
+a=[1,2,3]
+b=[1,2,3]
+print(id(a))
+print(id(b))
+
